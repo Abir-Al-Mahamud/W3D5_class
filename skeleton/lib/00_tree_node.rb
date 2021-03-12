ea52ed204e3f953bdc8b05bb3d135ce5efd5a99e
min <minwang.bus@gmail.com>
2021-03-12 17:17:06 -0500

    add add_child and remove_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -17,8 +17,25 @@ class PolyTreeNode
         new_node.children << self if !@parent.nil?
     end
 
-    # def
-    
+    def add_child(child_node)
+        child_node.parent = self
+    end
 
+    def remove_child(child_node)
+        if @children.include?(child_node)
+            child_node.parent = nil
+        else
+            raise "error"
+        end
+    end
+
+    def dfs(root, target)
+        return root if root == target
+
+        dfs(root, children)
+
+
+    end
     
 end
+
